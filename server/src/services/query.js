399a6adeb7bce16899100c1d2f13077297a9445a
Ahmad Nawar Droubi <nawar.droubi@gmail.com>
2024-07-21 16:55:48 +0300

    Fixed bug in skip calculations

--- server/src/services/query.js
+++ server/src/services/query.js
@@ -4,7 +4,8 @@ const DEFAULT_PAGE_NUMBER = 1;
 function getPagination(query) {
 	const limit = Math.abs(query.limit) || DEFAULT_PAGE_NUMBER;
 	const page = Math.abs(query.page) || DEFAULT_PAGE_LIMIT;
-	const skip = (page - 1) * limit;
+	let skip = (page - 1) * limit;
+	skip = skip < 0 ? 0 : skip;
 
 	return {
 		limit,
